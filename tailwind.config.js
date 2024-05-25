4442581dffc16738751a48fc3829bb3bbd23988c
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-05-25 22:51:37 +0700

    feat: init custom style setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-nutmeg": "hsl(14, 45%, 36%)",
+        "primary-dark-raspberry": "hsl(332, 51%, 32%)",
+        "neutral-white": "hsl(0, 0%, 100%)",
+        "neutral-rose-white": "hsl(330, 100%, 98%)",
+        "neutral-eggshell": "hsl(30, 54%, 90%)",
+        "neutral-wenge-brown": "hsl(30, 10%, 34%)",
+        "neutral-dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+    },
   },
   plugins: [],
 };
