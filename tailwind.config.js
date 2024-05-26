2d823ba8434cc25b801cc36692c6e5fd503fa9c6
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-05-26 22:57:56 +0700

    style: styling title article, preparation article, and set custom font Young Serif and Outfit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
       colors: {
         "primary-nutmeg": "hsl(14, 45%, 36%)",
         "primary-dark-raspberry": "hsl(332, 51%, 32%)",
+        "primary-light-raspberry": "hsl(332, 51%, 97%)",
         "neutral-white": "hsl(0, 0%, 100%)",
         "neutral-rose-white": "hsl(330, 100%, 98%)",
         "neutral-eggshell": "hsl(30, 54%, 90%)",
         "neutral-wenge-brown": "hsl(30, 10%, 34%)",
         "neutral-dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+      fontFamily: {
+        youngSerif: ['"Young Serif"', "serif"],
+        outfit: ['"Outfit"', "serif"],
       },
     },
   },
